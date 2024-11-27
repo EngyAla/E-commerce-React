52cd244de1de8c3511362dd22d2cd814545a92f0
engy <engy55428@gmail.com>
2024-11-28 00:54:08 +0200

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -26,6 +26,7 @@ function Navbar() {
         localStorage.removeItem("password")
     }
     const handelCart = (e)=>{
+        e.preventDefault();
         if(localStorage.getItem("username") && localStorage.getItem("email") && localStorage.getItem("password")){
             navigate('/cart')
         } else{
@@ -33,6 +34,7 @@ function Navbar() {
         }
     }
     const handelFav = (e)=>{
+        e.preventDefault();
         if(localStorage.getItem("username") && localStorage.getItem("email") && localStorage.getItem("password")){
             navigate('/favourite')
         } else{
@@ -96,20 +98,20 @@ return(
 
             <div className='d-flex gap-1 align-items-center'>
                 <div>
-                    <a href=''>
-                        <svg onClick={handelFav} width="23" height="23" className='fav' viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div>
+                        <svg style={{cursor: "pointer"}} onClick={handelFav} width="23" height="23" className='fav' viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M5 1C2.7912 1 1 2.73964 1 4.88594C1 6.61852 1.7 10.7305 8.5904 14.8873C8.71383 14.961 8.85552 15 9 15C9.14448 15 9.28617 14.961 9.4096 14.8873C16.3 10.7305 17 6.61852 17 4.88594C17 2.73964 15.2088 1 13 1C10.7912 1 9 3.35511 9 3.35511C9 3.35511 7.2088 1 5 1Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
-                    </a>
+                    </div>
                     <p className='fav_count'>{fav.length}</p>
                 </div>
                 <div>
-                    <a href=''><svg className='' onClick={handelCart} width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <div><svg style={{cursor: "pointer"}} className='' onClick={handelCart} width="26" height="26" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8.25 20.25C8.66421 20.25 9 19.9142 9 19.5C9 19.0858 8.66421 18.75 8.25 18.75C7.83579 18.75 7.5 19.0858 7.5 19.5C7.5 19.9142 7.83579 20.25 8.25 20.25Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M18.75 20.25C19.1642 20.25 19.5 19.9142 19.5 19.5C19.5 19.0858 19.1642 18.75 18.75 18.75C18.3358 18.75 18 19.0858 18 19.5C18 19.9142 18.3358 20.25 18.75 20.25Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M2.25 3.75H5.25L7.5 16.5H19.5" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
                             <path d="M7.5 12.5H19.1925C19.2792 12.5001 19.3633 12.4701 19.4304 12.4151C19.4975 12.3601 19.5434 12.2836 19.5605 12.1986L20.9105 5.44859C20.9214 5.39417 20.92 5.338 20.9066 5.28414C20.8931 5.23029 20.8679 5.18009 20.8327 5.13717C20.7975 5.09426 20.7532 5.05969 20.703 5.03597C20.6528 5.01225 20.598 4.99996 20.5425 5H6" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg> </a>
+                        </svg> </div>
                     <p className="cart_count">{cart.length}</p>
                 </div>
                 <div class="dropdown">
